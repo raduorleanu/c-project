24090023421f13bb6807db0c0a2eb373e428bbef
Radu Orleanu <radu.orleanu@gmail.com>
2018-04-19 09:37:23 +0200

    initial commit with drivers and example test

---
+++ cyrprojct/cyrprojct/main.c
@@ -0,0 +1,20 @@
+/*
+ * cyrprojct.c
+ *
+ * Created: 4/19/2018 9:19:24 AM
+ * Author : Radu
+ */ 
+
+#include <avr/io.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
+#include "atmega2560_drivers.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
